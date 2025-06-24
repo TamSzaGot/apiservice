66f3fda2460de6950383eda4bff7b5760d0cee76
Tamas Szabo <tamas.szabo@arbetsformedlingen.se>
2025-06-24 08:15:28 +0200

    feat: add openapi yaml and minimal program

---
+++ Program.cs
@@ -0,0 +1,7 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Run();
+
+// This is a minimal ASP.NET Core application that does nothing.
+// It is designed to be a starting point for building web applications.
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # apiservice
 Example API service C# implementation
+
+# Specification first
+Start with an openapi specification and generate server side code
